94b766d91ab7229a57a1328ed3483024a096e9c3
halbotaku <lottchensrps@gmail.com>
2017-04-22 09:43:42 +0200

    Fixing WayPointSystem w/ Ronja
    Es tut jetzt.

--- Spiel/Assets/Scripts/Paths/pathFollower.cs
+++ Spiel/Assets/Scripts/Paths/pathFollower.cs
@@ -18,9 +18,19 @@ public class pathFollower : MonoBehaviour {
     //Create booleans for start/stop of Movement
     public bool rand = false;
     public bool go = true;
-    
-	// Use this for initialization
-	void Start () {
+
+    // variable to hold a reference to our SpriteRenderer component (Flipping the Sprite)
+    private SpriteRenderer mySpriteRenderer;
+
+    // This function is called just one time by Unity the moment the game loads
+    private void Awake()
+    {
+        // get a reference to the SpriteRenderer component on this gameObject (Flipping the Sprite)
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
@@ -68,7 +78,31 @@ public class pathFollower : MonoBehaviour {
 
     public void Move()
     {
-        gameObject.transform.LookAt(waypoints[num].transform.position);
-        gameObject.transform.position += gameObject.transform.forward * speed * Time.deltaTime;
+        //Get the difference of the current position to the next waypoint
+        Vector3 difference = waypoints[num].transform.position-transform.position;
+
+        //calculate the normal vector
+        difference.Normalize();
+
+        //move the object from current to next waypoint
+        gameObject.transform.position += difference * speed * Time.deltaTime;
+
+
+        // if the variable isn't empty (we have a reference to our SpriteRenderer)
+        if (mySpriteRenderer != null)
+        {
+            //When movement is going to the left
+            if (difference.x < 0)
+            {
+                // flip the sprite
+                mySpriteRenderer.flipX = true;
+            }
+            else if (difference.x > 0)
+            {
+                // revert the sprite to normal
+                mySpriteRenderer.flipX = false;
+            }
+        }
+
     }
 }
